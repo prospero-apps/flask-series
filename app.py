b872d7d6633eb03a9f64c67fd08de93f1058ada0
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2024-12-28 20:38:47 +0100

    code for part 4

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, send_file
+import pandas as pd
+import os
 
 app = Flask(__name__, template_folder='templates')
+
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -23,6 +29,49 @@ def fun_stuff():
 @app.route('/go_home')
 def go_home():
     return redirect(url_for('index'))
+
+@app.route('/personal_info', methods=['GET', 'POST'])
+def personal_info():
+    if request.method == 'GET':
+        return render_template('personal_info.html')
+    elif request.method == 'POST':
+        if 'username' in request.form.keys():
+            username = request.form['username']
+        if 'password' in request.form.keys():
+            password = request.form['password']
+
+        if username == 'prospero' and password == 'coder':
+            return "You're logged in."
+        else:
+            return "Wrong username or password!"
+        
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    if 'file' not in request.files:
+        return 'Nothing to display.', 400
+    
+    file = request.files['file']
+    filename = file.filename
+
+    if filename == '':
+        return 'No file selected!', 400
+    
+    if file.content_type.startswith('image/'):
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return render_template('photo.html', image_url=f"/uploads/{filename}")
+    elif file.content_type == 'text/csv':
+        df = pd.read_csv(file)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)        
+        return df.to_html()
+    
+    return 'Unsupported file format!', 400
+
+@app.route('/uploads/<filename>')
+def serve_image(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_file(filepath, mimetype='image/*')
 
 @app.template_filter('every_other_letter')
 def every_other_letter(word):
