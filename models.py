5cf762bdd79ba4072aec28b700c4f93a28be0e81
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2025-01-09 16:37:48 +0100

    code for part 8

--- models.py
+++ models.py
@@ -1,12 +1,17 @@
 from app import db
+from flask_login import UserMixin
 
-class Student(db.Model):
-    __tablename__ = 'students'
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.Text, nullable=False)
-    last_name = db.Column(db.Text, nullable=False)
-    age = db.Column(db.Integer)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    profile = db.Column(db.String)
+    role = db.Column(db.String)
 
     def __repr__(self):
-        return f'{self.first_name} {self.last_name}, {self.age}'
+        return f'{self.username} ({self.role})'
+    
+    def get_id(self):
+        return self.id
--- routes.py
+++ routes.py
@@ -1,40 +1,50 @@
-from flask import render_template, request
-from models import Student
+from flask import render_template, request, redirect, url_for
+from models import User
+from flask_login import login_user, logout_user, login_required, current_user
 
-def register_routes(app, db):
+def register_routes(app, db, bcrypt):
 
-    @app.route('/', methods=['GET', 'POST'])
+    @app.route('/')
     def index():
-        if request.method == 'GET':
-            students = Student.query.all()
-            return render_template('index.html', students=students)
-        elif request.method == 'POST':
-            first_name = request.form.get('firstname')
-            last_name = request.form.get('lastname')
-            age = int(request.form.get('age'))
-
-            student = Student(
-                first_name=first_name,
-                last_name=last_name,
-                age=age
-            )
-
-            db.session.add(student)
-            db.session.commit()
-
-            students = Student.query.all()
-            return render_template('index.html', students=students)
-        
-    @app.route('/delete/<id>', methods=['DELETE'])
-    def delete(id):
-        Student.query.filter(Student.id == id).delete()
-
-        db.session.commit()
-
-        students = Student.query.all()
-        return render_template('index.html', students=students)
+        return render_template('index.html')
     
-    @app.route('/details/<id>')
-    def details(id):
-        student = Student.query.filter(Student.id == id).first()
-        return render_template('details.html', student=student)
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'GET':
+            return render_template('signup.html')
+        elif request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+            password_hash = bcrypt.generate_password_hash(password)
+            user = User(username=username, password=password_hash)
+
+            db.session.add(user)
+            db.session.commit() 
+
+            return redirect(url_for('index'))
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            user = User.query.filter(User.username == username).first()
+
+            if user and bcrypt.check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                return 'Incorrect password or username!'            
+    
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
+    
+    @app.route('/not_for_everyone')
+    @login_required
+    def not_for_everyone():
+        return 'TOP SECRET'
